03eed16cbbcd94bcdbbdd877965ec750661625ab
Florian Wessner <info@wesflo.de>
2017-01-12 16:44:43 +0200

    some recipe stylings

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'index'
     <?php include_once 'template/_partials/layout/head.phtml'; ?>
 </head>
 <body>
-<div class="cntWrap" id="cntWrap">
-    <?php include_once 'template/' . $page . '.phtml'; ?>
-</div>
+<?php include_once 'template/' . $page . '.phtml'; ?>
 <?php
 include_once 'template/_partials/layout/header.phtml';
 include_once 'template/_partials/layout/footer.phtml';
